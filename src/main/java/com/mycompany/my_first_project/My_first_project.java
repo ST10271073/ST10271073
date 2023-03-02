d3cdcbc90d78df9e372d911439efef4e5b2db197
Thomas Fraser <ST10271073@vcconnect.edu.za>
2023-03-02 12:40:30 +0200

    My first commit

---
+++ src/main/java/com/mycompany/my_first_project/My_first_project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.my_first_project;
+
+/**
+ *
+ * @author tfras
+ */
+public class My_first_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
